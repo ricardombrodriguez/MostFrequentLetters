6123d850235eb7ea5767bda3c21c9892994cd83a
Ricardo Rodriguez <ricardorodriguez@ua.pt>
2022-12-27 22:27:49 +0000

    process text file and remove gutenbergs headers/footers

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 import collections
 import os
+import re
 import string
+import advertools as adv
+
 
 def compute_exact_counts(text):
-    # Compute the exact number of occurrences of each letter
+    # Compute the exact number of occurrences of each letter with collections library
     letter_counts = collections.Counter(text.lower())
     return letter_counts
+
+def process_file(filename):
+    with open(f'files/{filename}', 'r') as f:
+        text = f.read()
+        # Extract the language from the Project Gutenberg's file using a regular expression
+        regex = r'Language: ([a-zA-Z]+)'
+        match = re.search(regex, text)
+        # Find the start and end indexes of header and footer for removal and extract the body
+        header_end = text.find('*** START OF THIS PROJECT GUTENBERG EBOOK')
+        footer_start = text.find('*** END OF THIS PROJECT GUTENBERG EBOOK')
+        text = text[header_end+len('*** START OF THIS PROJECT GUTENBERG EBOOK'):footer_start+len('*** END OF THIS PROJECT GUTENBERG EBOOK')]
+        # Removing stopwords, punctuation marks and convert text to uppercase
+        stopwords = []
+        if match:
+            language = match.group(1).lower()
+            stopwords = set(adv.stopwords[language])
+        text = ' '.join([word for word in text.split() if word.lower() not in stopwords])
+        text = text.translate(str.maketrans('', '', string.punctuation)).upper()
+    return text
 
 def estimate_frequent_letters(text, k):
     # Estimate the k most frequent letters using a data stream algorithm
@@ -21,9 +43,10 @@ def estimate_frequent_letters(text, k):
     return letter_counts
 
 def test_estimate_frequent_letters(text, k):
-    # Perform a set of tests to evaluate the quality of estimates
+    # Start by computing the exact count for future comparison with estimates
     exact_counts = compute_exact_counts(text)
     total_error = 0
+    # Repeat the approximate count 10 times:
     for i in range(10):
         estimate_counts = estimate_frequent_letters(text, k)
         print(estimate_counts)
@@ -42,11 +65,9 @@ def compare_performance(text):
 
 def main():
     for filename in os.listdir('files'):
-        print(filename)
-        with open(f'files/{filename}', 'r') as f:
-            text = f.read()
-            errors = compare_performance(text)
-            print(errors)
+        text = process_file(filename)
+        errors = compare_performance(text)
+        print(errors)
 
 if __name__ == '__main__':
     main()
